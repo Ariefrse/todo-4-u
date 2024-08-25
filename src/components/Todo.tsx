6a6c79ceffa8edfa65d325225d069c3522a814bb
ariefrse <144644432+Ariefrse@users.noreply.github.com>
2024-08-25 18:19:42 +0000

    latest update

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,18 +1,17 @@
 import { useEffect, useState } from "react";
-import "./Todo.css"; 
+import "./Todo.css";
 import TodoItem from "./TodoItem";
 
 interface Task {
-    id: number;
-    task: string;
-    completed: boolean;
-    lastUpdated: string;
-  }
+  id: number;
+  task: string;
+  completed: boolean;
+  lastUpdated: string;
+}
 
 const Todo = () => {
   const [task, setTask] = useState<Task[]>([]);
   const [todo, setTodo] = useState<string>("");
-  
 
   useEffect(() => {
     const stored = JSON.parse(localStorage.getItem("todo") || "[]");
@@ -32,24 +31,35 @@ const Todo = () => {
 
   const addTodo = () => {
     if (todo.trim() !== "") {
-        const newTask = { id: Date.now(), task: todo, completed: false, lastUpdated: new Date().toLocaleString() };
+      const newTask = {
+        id: Date.now(),
+        task: todo,
+        completed: false,
+        lastUpdated: new Date().toLocaleString(),
+      };
       const updatedTasks = [...task, newTask];
       updateLocalStorage(updatedTasks);
       setTodo("");
     }
   };
 
-  const deleteTodo = (id: number ) => {
+  const deleteTodo = (id: number) => {
     const updatedTasks = task.filter((item) => item.id !== id);
     updateLocalStorage(updatedTasks);
   };
 
   const toggleTodo = (id: number) => {
     const updatedTasks = task.map((item) =>
-        item.id === id ? { ...item, completed: !item.completed, lastUpdated: new Date().toLocaleString() } : item // Update this line
+      item.id === id
+        ? {
+            ...item,
+            completed: !item.completed,
+            lastUpdated: new Date().toLocaleString(),
+          }
+        : item,
     );
     updateLocalStorage(updatedTasks);
-};
+  };
 
   return (
     <div className="todo-container">
@@ -60,14 +70,13 @@ const Todo = () => {
           className="input-todo"
           value={todo}
           onChange={(e) => setTodo(e.target.value)}
-          placeholder="Add new list here" 
+          placeholder="Add new list here"
         />
         <button className="todo-btn" onClick={addTodo}>
           Add Todo
         </button>
       </div>
       <div className="todo-list">
-       
         <ul>
           {task.map((item, index) => (
             <TodoItem
--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,51 +1,43 @@
-import PropTypes from "prop-types";
-import "./TodoItem.css";
+import "./Todo.css";
 
 interface Task {
-    id: number;
-    task: string;
-    completed: boolean;
-    lastUpdated: string;
-  }
-  
-  interface TodoItemProps {
-    item: Task;
-    onDelete: (id: number) => void;
-    onToggle: (id: number) => void;
-    className: string;
-    index: number;
-  }
+  id: number;
+  task: string;
+  completed: boolean;
+  lastUpdated: string;
+}
 
+interface TodoItemProps {
+  item: Task;
+  onDelete: (id: number) => void;
+  onToggle: (id: number) => void;
+  className: string;
+  index: number;
+}
 
-  const TodoItem: React.FC<TodoItemProps> = ({ item, onDelete, onToggle, index, className }) => {
-    return (
-        <li className={className} title={`Task added: ${item.lastUpdated}`}>
-
-     
-        <p className={item.completed ? "completed" : ""}>
+const TodoItem: React.FC<TodoItemProps> = ({
+  item,
+  onDelete,
+  onToggle,
+  index,
+  className,
+}) => {
+  return (
+    <li className={className} title={`Task added: ${item.lastUpdated}`}>
+      <p className={item.completed ? "completed" : ""}>
         {index + 1}. {item.task}
-        </p>
-        <input
-          type="checkbox"
-          className="custom-checkbox"
-          checked={item.completed}
-          onChange={() => onToggle(item.id)}
-        />
-        <button className="delete-btn" onClick={() => onDelete(item.id)}>X</button>
-       
-      </li>
-    );
-  };
-TodoItem.propTypes = {
-  item: PropTypes.shape({
-    id: PropTypes.number.isRequired,
-    task: PropTypes.string.isRequired,  
-    completed: PropTypes.bool.isRequired,
-    lastUpdated: PropTypes.string.isRequired,
-  }).isRequired,
-  onDelete: PropTypes.func.isRequired,
-  onToggle: PropTypes.func.isRequired,
-  
+      </p>
+      <input
+        type="checkbox"
+        className="custom-checkbox"
+        checked={item.completed}
+        onChange={() => onToggle(item.id)}
+      />
+      <button className="same-size" onClick={() => onDelete(item.id)}>
+        X
+      </button>
+    </li>
+  );
 };
 
 export default TodoItem;
